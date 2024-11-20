26c0c40964a9c293a52ac9f6951df5ce4262d57b
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-11-20 08:49:26 +0600

    First commit

---
+++ src/main/java/com/aleksandr_kobelskiy/week12practice/ItEchoAleksandrKobelskiyWeek12PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.aleksandr_kobelskiy.week12practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItEchoAleksandrKobelskiyWeek12PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItEchoAleksandrKobelskiyWeek12PracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aleksandr_kobelskiy/week12practice/ItEchoAleksandrKobelskiyWeek12PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksandr_kobelskiy.week12practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItEchoAleksandrKobelskiyWeek12PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
