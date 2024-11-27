374ddc108492ec293be1b660e1e3c53d3b8fd13a
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-11-27 16:57:19 +0600

    Добавил начальные зависимости для AWS.

---
+++ src/main/java/com/aleksandr_kobelskiy/week12practice/config/S3ClientConfig.java
@@ -0,0 +1,13 @@
+//package com.aleksandr_kobelskiy.week12practice.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class S3ClientConfig {
+//
+//    @Bean
+//    public S3AsyncClient s3AsyncClient() {
+//
+//    }
+//}
