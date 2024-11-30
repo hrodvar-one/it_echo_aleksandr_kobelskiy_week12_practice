ebcb34ccc2ca5cb69037c450c1bc5bdecf934a96
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-11-30 19:09:45 +0600

    Добавил скрипт миграции БД в котором инициируется со старта пользователь root с ролью ADMIN.

---
+++ src/main/resources/db/migration/V4__insert_initial_users_in_users_table.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (username, password, role, first_name, last_name, status, created_at, updated_at)
+VALUES (
+    'root',
+    'KxMPdnDmXTsUCr11YAYEjk7ncRrCihqB9z/Knp7c8bU=',
+    'ADMIN',
+    'System',
+    'Administrator',
+    'ACTIVE',
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+);
